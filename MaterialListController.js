ec0f85c472fd7f008ed944d3edb435f466c8ae22
Mariana Lima <81395468+marimagalhaesl@users.noreply.github.com>
2023-04-21 21:22:52 -0300

    Create MaterialListController.js

---
+++ MaterialListController.js
@@ -0,0 +1,16 @@
+({
+    doInit: function(component, event, helper) {
+        helper.doInitHelper(component);
+    },
+    
+    handleSuccess: function(component, event, helper) {
+        helper.handleSuccessHelper(component);
+    },
+    
+    handleReset: function(component, event, helper) {
+        helper.handleResetHelper(component);
+    }
+    
+    
+    
+})
